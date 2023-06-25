b1202de81030febd4cfd5ff63395226c890a4fb9
Leesuyeoun <sssiuir@gmail.com>
2023-06-25 21:45:57 +0900

    Cex2

--- Cex2/main.c
+++ Cex2/main.c
@@ -1,14 +1,17 @@
 //
 //  main.c
 //  Cex2
-//
+// 전역변수
 //  Created by 이수연 on 2023/06/25.
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int a = 5;
+void fn(){
+    a = a+3;
+}
+void main (){
+    a = a+5;
+    fn();
+    printf("%d\n", a);
 }
